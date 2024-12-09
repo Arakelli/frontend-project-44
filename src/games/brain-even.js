375e0aedc12133188a4ebe7f7aa367a0fb891342
Gas <gas.gasan2000@yandex.ru>
2024-12-09 10:45:44 +0500

    update brain-even.js

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,36 +1,18 @@
-import readlineSync from 'readline-sync';
+import mainEngine from "../index.js";
+import randomIntFromInterval from "../getRandomNumber.js";
 
-function randomIntFromInterval(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min);
-};
 
 const isEven = (num) => num % 2 === 0 ? 'yes' : 'no';
 
-const startEven = () => {
-    console.log('Welcome to the Brain Games!');
-    let name = readlineSync.question('May I have your name? ')
-    console.log(`Hello, ${name}!`);
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-    const roundCounts = 3;
+const getData = () => {
+    const question = randomIntFromInterval(0, 100);
+    const questionRigth = isEven(question);
 
-    for (let i = 1; i <= roundCounts; i += 1) {
-        const randomNumber = randomIntFromInterval(0, 100);
-        const questionRigth = isEven(randomNumber);
-
-        console.log(`Question: ${randomNumber}`);
-        const answer = readlineSync.question('You answer: ');
-
-        if (questionRigth === answer) {
-            console.log('Correct!');
-        } else {
-            console.log(`'${answer}' is wrong answer ;(. Correct answer was '${questionRigth}'`);
-            console.log(`Let's try again ${name}`);
-            return;
-        }
-    }
-
-    console.log(`Congratulations, ${name}`);
+    return [question, questionRigth];
 };
 
-export default startEven;
+const startEvenGame = () => mainEngine(getData, description);
+
+export default startEvenGame;
