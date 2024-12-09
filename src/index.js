4d08f3440b222db831153f7e43f5c560dda039b5
Gas <gas.gasan2000@yandex.ru>
2024-12-09 14:14:34 +0500

    add index.js

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import readlineSync from 'readline-sync';
+
+const mainEngine = (getData, description) => {
+    console.log('Welcome to the Brain Games!');
+    let name = readlineSync.question('May I have your name? ')
+    console.log(`Hello, ${name}!`);
+    
+    console.log(description)
+
+    const roundCounts = 3;
+
+    for (let i = 1; i <= roundCounts; i += 1) {
+        const [question, questionRigth] = getData();
+
+        console.log(`Question: ${question}`);
+        const answer = readlineSync.question('You answer: ');
+
+        if (questionRigth === answer) {
+            console.log('Correct!');
+        } else {
+            console.log(`'${answer}' is wrong answer ;(. Correct answer was '${questionRigth}'`);
+            console.log(`Let's try again ${name}`);
+            return;
+        }
+    }
+
+    console.log(`Congratulations, ${name}`);
+};
+
+export default mainEngine;
