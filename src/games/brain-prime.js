4579d5567b2d8a35892d659bb753cceb1e96e3c2
Gas <gas.gasan2000@yandex.ru>
2024-12-10 16:32:14 +0500

    fix brain-prime.js

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,23 +1,23 @@
-import mainEngine from "../index.js";
-import randomIntFromInterval from "../getRandomNumber.js";
+import mainEngine from '../index.js';
+import randomIntFromInterval from '../getRandomNumber.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
-    for (let i = 2; i <= number / 2; i += 1) {
-        if (number % i === 0) {
-            return 'no';
-        }
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) {
+      return 'no';
     }
+  }
 
-    return 'yes';
+  return 'yes';
 };
 
 const getData = () => {
-    const question = randomIntFromInterval(2, 100);
-    const questionRigth = isPrime(question);
+  const question = randomIntFromInterval(2, 100);
+  const questionRigth = isPrime(question);
 
-    return [question, questionRigth];
+  return [question, questionRigth];
 };
 
 const startPrimeGame = () => mainEngine(getData, description);
