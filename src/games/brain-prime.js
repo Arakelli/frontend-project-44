f485f258f36b3f5dccd4b10d7c19bf68158facae
Gas <gas.gasan2000@yandex.ru>
2024-12-10 15:01:58 +0500

    update brain-prime.js

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,7 +1,7 @@
 import mainEngine from "../index.js";
 import randomIntFromInterval from "../getRandomNumber.js";
 
-const description = 'Answer "yes" if given is prome. Otherwise answer "no".';
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
     for (let i = 2; i <= number / 2; i += 1) {
