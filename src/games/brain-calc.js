80dae7cbf45f20b2d5db7ec139242ca408dc39e1
Gas <gas.gasan2000@yandex.ru>
2024-12-10 16:31:43 +0500

    fix brain-calc.js

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,5 +1,5 @@
-import mainEngine from "../index.js";
-import randomIntFromInterval from "../getRandomNumber.js";
+import mainEngine from '../index.js';
+import randomIntFromInterval from '../getRandomNumber.js';
 
 const description = 'What is the result of the expression?';
 
@@ -7,26 +7,26 @@ const operators = ['+', '-', '*'];
 const randomIndex = (data) => Math.floor(Math.random() * data.length);
 
 const isCalc = (num1, num2, symbol) => {
-    switch (symbol) {
-        case '+':
-            return num1 + num2;
-        case '-':
-            return num1 - num2;
-        case '*':
-            return num1 * num2;
-        default:
-            return null;
+  switch (symbol) {
+    case '+':
+        return num1 + num2;
+    case '-':
+        return num1 - num2;
+    case '*':
+        return num1 * num2;
+    default:
+        return null;
     }
 };
 
 const getData = () => {
-    const number1 = randomIntFromInterval(0, 100);
-    const number2 = randomIntFromInterval(0, 100);
-    const operator = operators[randomIndex(operators)];
-    const question = `${number1} ${operator} ${number2}`;
-    const questionRigth = isCalc(number1, number2, operator).toString();
+  const number1 = randomIntFromInterval(0, 100);
+  const number2 = randomIntFromInterval(0, 100);
+  const operator = operators[randomIndex(operators)];
+  const question = `${number1} ${operator} ${number2}`;
+  const questionRigth = isCalc(number1, number2, operator).toString();
 
-    return [question, questionRigth];
+  return [question, questionRigth];
 };
 
 const startCalcGame = () => mainEngine(getData, description);
