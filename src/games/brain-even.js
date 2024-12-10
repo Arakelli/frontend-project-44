b0d148b36adcfc95dc73bed85bcd514dc6710804
Gas <gas.gasan2000@yandex.ru>
2024-12-10 16:31:54 +0500

    fix brain-even.js

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,15 +1,15 @@
-import mainEngine from "../index.js";
-import randomIntFromInterval from "../getRandomNumber.js";
+import mainEngine from '../index.js';
+import randomIntFromInterval from '../getRandomNumber.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (num) => num % 2 === 0 ? 'yes' : 'no';
+const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
 
 const getData = () => {
-    const question = randomIntFromInterval(0, 100);
-    const questionRigth = isEven(question);
+  const question = randomIntFromInterval(0, 100);
+  const questionRigth = isEven(question);
 
-    return [question, questionRigth];
+  return [question, questionRigth];
 };
 
 const startEvenGame = () => mainEngine(getData, description);
