95f6511ea2afd62b035c39851ea349b892abbe72
Gas <gas.gasan2000@yandex.ru>
2024-12-12 16:25:18 +0500

    fix brainEven.js

--- src/games/brainEven.js
+++ src/games/brainEven.js
@@ -3,7 +3,7 @@ import getRandomNumber from '../getRandomNumber.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (num) => num % 2 === 0 ? 'yes' : 'no';
+const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
 
 const getData = () => {
   const question = getRandomNumber(0, 100);
