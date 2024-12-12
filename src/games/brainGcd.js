673b1732716840f4f4fca5c16b74b1e713b5812c
Gas <gas.gasan2000@yandex.ru>
2024-12-12 16:18:37 +0500

    fix brainGcd.js

---
+++ src/games/brainGcd.js
@@ -0,0 +1,19 @@
+import runGame from '../index.js';
+import getRandomNumber from '../getRandomNumber.js';
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+const calculate = (num1, num2) => (num2 === 0 ? num1 : calculate(num2, num1 % num2));
+
+const getData = () => {
+  const number1 = getRandomNumber(0, 100);
+  const number2 = getRandomNumber(0, 100);
+  const question = `${number1} ${number2}`;
+  const answerRigth = calculate(number1, number2).toString();
+
+  return [question, answerRigth];
+};
+
+const startGCDGame = () => runGame(getData, description);
+
+export default startGCDGame;
