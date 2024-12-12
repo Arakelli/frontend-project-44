757818591d4f415288ff8b9e914b8a3c1430d9c1
Gas <gas.gasan2000@yandex.ru>
2024-12-12 16:18:59 +0500

    fix brainProgression.js

---
+++ src/games/brainProgression.js
@@ -0,0 +1,31 @@
+import runGame from '../index.js';
+import getRandomNumber from '../getRandomNumber.js';
+
+const description = 'What number is missing in the progression?';
+
+const getArithmeticProgression = (length, start, step) => {
+  const progression = [];
+  for (let i = start; progression.length < length; i += step) {
+    progression.push(i);
+  }
+  return progression;
+};
+
+const getRandomIndex = (data) => Math.floor(Math.random() * data.length);
+
+const getData = () => {
+  const length = getRandomNumber(5, 10);
+  const start = getRandomNumber(0, 100);
+  const step = getRandomNumber(1, 10);
+  const progression = getArithmeticProgression(length, start, step);
+  const randomIndex = getRandomIndex(progression);
+  const answerRigth = progression[randomIndex].toString();
+  progression[randomIndex] = '..';
+  const question = progression.join(' ');
+
+  return [question, answerRigth];
+};
+
+const startProgressionGame = () => runGame(getData, description);
+
+export default startProgressionGame;
